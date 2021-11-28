25d964393969c3fb48ccb1ba60841cd5c662c827
David Ecsery <david@galatia.com>
2021-11-28 09:03:01 +0000

    Corrections

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ function compute() {
     var principal = document.getElementById("principal").value;
     if (principal <= 0) {
         alert("Enter a positive number");
-        principal.focus();
+        document.getElementById("principal").focus();
     }
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate / 100;
     var year = new Date().getFullYear() + parseInt(years);
-    document.getElementById("result").innerText.innerText = "If you deposit " + principal
+    document.getElementById("result").innerHTML = "If you deposit " + principal
     + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of "
-    + amount + ",\<br\>in the year " + year + "\<br\>";
+    + interest + ",\<br\>in the year " + year + "\<br\>";
 }
 
 function updateRate() {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val") = rateval;
+    document.getElementById("rate_val").innerText = rateval;
 }
 
         
