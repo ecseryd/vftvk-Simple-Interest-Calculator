562a3f9668ff7fd8e8fbf5d8cca890074f1c2563
David Ecsery <david@galatia.com>
2021-11-28 10:10:55 +0000

    Avoid printing the interest message after a principal <= 0 alert

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ function compute() {
     var principal = document.getElementById("principal").value;
     if (principal <= 0) {
         alert("Enter a positive number");
+        document.getElementById("result").innerHTML = "";
         document.getElementById("principal").focus();
+        return;
     }
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
