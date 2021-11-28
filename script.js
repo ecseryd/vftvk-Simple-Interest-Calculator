804f1c50f046b79b1d01d14a39c2ef13712ebabf
David Ecsery <david@galatia.com>
2021-11-28 08:39:33 +0000

    More output

--- script.js
+++ script.js
@@ -9,7 +9,9 @@ function compute() {
 
 function updateRate() {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText = rateval;
+    document.getElementById("rate_val").innerText = "If you deposit " + principal
+        + ",\<br\>at an interest rate of " + rate + "%\<br\>You will receive an amount of "
+        + amount + ",\<br\>in the year " + year + "\<br\>";
 }
 
         
